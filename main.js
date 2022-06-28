8c617d73ffe9929e9aac16f679271f3862ba9232
sy-uname1968 <67422892+sy-uname1968@users.noreply.github.com>
2022-06-28 12:17:30 +0300

    First commit

---
+++ main.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+
+const advice_url = "https://api.adviceslip.com/advice"
+const quoteBody = document.querySelector(".advice-body")
+const quoteCounter = document.querySelector(".advice-number")
+const adviceButton = document.querySelector(".advice-btn")
+
+const getAdvice = async() => {
+ 
+  const options = {
+    headers: { 
+            "Accept" : "application/json",
+            "Accept-Encoding": "gzip, deflate, br"
+    }
+  }
+
+  try {    
+    const { data } = await axios.get(advice_url, null, options )
+    quoteCounter.textContent = data.slip.id
+    quoteBody.textContent = data.slip.advice
+    console.log(data.slip)
+    // return {access_token: data.access_token}
+  }
+  catch(error) {
+    console.log(error)
+    // const {message, name, stack, code, status} = error
+    // return {access_token: null, err: {message, name, stack, code, status} };
+  }
+
+}
+
+adviceButton.addEventListener("click", (e) => {
+  getAdvice()
+})
+
+
+getAdvice()
